60475683a0dd7e3906417fb0f51c5994fa74a6d6
Adelia <adeliasmilecontact@gmail.com>
2023-11-28 22:29:33 +0700

    day-2

---
+++ theory/theory-1-introduction.py
@@ -0,0 +1,38 @@
+"""
+- Dibuat dan dikembangkan oleh Guido Van Rossum, dari Belanda. 
+- Pemilihan nama python itu sendiri diambil dari sebuah acara televisi yang lumayan terkenal yang bernama Moth Python Flying Circus yang merupakan acara sirkus favorit dari Guido.
+
+Kelebihan diatara python antara lain :
+- Mudah digunakan untuk menulis di perangkat lunak terbaru
+- Mudah untuk diajarkan
+- Mendukung IoT (Internet of Things)
+- Mudah dipahami
+- Serta mudah didapatkan, karena mudah diinstal
+- Multiplatform
+
+Induknya python => Bahasa C
+
+Cek versi python --version
+
+Cara jalanin :
+- klik play
+- python nama-file.py (python theory(folder)/theory-1-introduction.py)
+
+Alur : Python => Bahasa pemrograman interpreted
+((versi interpreted))
+source code->intepreter(langsung diterjemahkan)->terminal //di eksekusi sebaris-sebaris
+Di eksekusi dari urutan atas ke bawah, klo baris kosong bakal dilewatin
+((versi compile)) 
+Bisa di compile kek .exe dengan bytecode 
+->caranya python -m py_compile nama file abis itu cd nama folder then jalanin filenya
+Pakai yang compile lebih cepat dari intepreted
+
+Komen : # versi sebaris ajah
+"""
+
+print('hello everybadiw')
+
+a="cantik"
+b="adel"
+print(b)
+print('hello everybadih')
